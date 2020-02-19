7a44e4405246b8927c1c4033062b31719b16756a
Vegctrp <kem.altair@gmail.com>
2020-02-19 19:55:36 +0900

    nuxtjs init

---
+++ mywebapp/layouts/default.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+
+.button--blue {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #0000ff;
+  color: #0000ff;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--blue:hover {
+  color: #fff;
+  background-color: #0000ff;
+}
+
+.button--red {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #ff0000;
+  color: #ff0000;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--red:hover {
+  color: #fff;
+  background-color: #ff0000;
+}
+
+.button--udukikoh {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #F9E97A;
+  color: #F9E97A;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--udukikoh:hover {
+  color: #fff;
+  background-color: #F9E97A;
+}
+</style>
---
+++ mywebapp/pages/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        Altair626's page
+      </h1>
+      <h2 class="subtitle">
+        なんかやる
+      </h2>
+      <div class="links">
+        <a
+          href="http://wiki.altair626.work:2500"
+          target="_blank"
+          class="button--green"
+        >
+          Wiki
+        </a>
+        <a
+          href="https://github.com/Vegctrp"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a><br>
+        <a
+          href="https://www.youtube.com/channel/UC3lNFeJiTq6L3UWoz4g1e-A"
+          target="_blank"
+          class="button--udukikoh"
+        >
+          卯月コウ
+        </a>
+        <a
+          href="https://twitter.com/mya626"
+          target="_blank"
+          class="button--blue"
+        >
+          Twitter
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
